ff3adc21b1edef112e26911b1c3d24ac3f5f57bd
MiiMii1205 <boldje1.work@gmail.com>
2025-12-16 21:57:38 -0500

    Added boost and lunge support to High Jumps

--- src/CanadianCuisine/CuisineCharacterPatcher.cs
+++ src/CanadianCuisine/CuisineCharacterPatcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CanadianCuisine.controllers;
 using HarmonyLib;
+using Photon.Pun;
 using pworld.Scripts.Extensions;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -14,13 +15,13 @@ public class CuisineCharacterPatcher
     public static void AfflictionAwakePostfix(CharacterAfflictions __instance)
     {
         var affliction = __instance.character.gameObject.GetOrAddComponent<CuisineAfflictionCharacter>();
-        
+
         if (affliction == null)
         {
             Plugin.Log.LogError("Can't add custom affliction manager");
         }
     }
-        
+
     [HarmonyPatch(typeof(GUIManager), "Awake")]
     [HarmonyPostfix]
     public static void GUIAwakePostfix(GUIManager __instance)
@@ -28,7 +29,7 @@ public class CuisineCharacterPatcher
         var man = __instance.gameObject.GetOrAddComponent<CuisineGUIEffectManager>();
 
         var effectTrs = __instance.poisonSVFX.transform;
-        
+
         if (man == null)
         {
             Plugin.Log.LogError("Can't add custom GUI manager");
@@ -43,5 +44,53 @@ public class CuisineCharacterPatcher
             effects.SetActive(false);
         }
     }
+
+    [HarmonyPatch(typeof(CharacterMovement), "CheckForPalJump")]
+    [HarmonyPrefix]
+    public static void CheckForPalJumpPrefix(CharacterMovement __instance, Character c, ref bool __runOriginal)
+    {
+        if (c.GetComponent<CuisineAfflictionCharacter>() is not {HasHighJump: true})
+        {
+            __runOriginal = true;
+        }
+        else
+        {
+            __runOriginal = false;
+            if (__instance.character.data.sinceStandOnPlayer < 0.3f && c.data.sinceJump < 0.3f)
+            {
+                __instance.character.data.lastStoodOnPlayer = null;
+                if (__instance.character.refs.view.IsMine)
+                {
+                    // This way we can call our custom High Jump RPC function that uses the character impulse as a base 
+                    __instance.character.refs.view.RPC("HighJumpRpc", RpcTarget.All, true, c.refs.movement.jumpImpulse);
+                }
+            }
+        }
+    }
     
+    [HarmonyPatch(typeof(CharacterClimbing), "RPCA_ClimbJump")]
+    [HarmonyPrefix]
+    public static void RPCA_ClimbJumpPrefix(CharacterClimbing __instance, ref bool __runOriginal)
+    {
+        if (__instance.GetComponent<CuisineAfflictionCharacter>() is not {HasHighJump: true} cac)
+        {
+            __runOriginal = true;
+        }
+        else
+        {
+            __runOriginal = false;
+            // OG Code
+            __instance.character.data.sinceClimbJump = 0f;
+            __instance.character.UseStamina(0.2f);
+            
+            // Multiply by a fraction of the high jump multiplier so the lung stays proportional to the jump height
+            __instance.playerSlide += __instance.character.input.movementInput.normalized * (8f *
+                (cac.highJumpMultiplier / 4f));
+            if (__instance.view.IsMine && !__instance.character.isBot)
+            {
+                GamefeelHandler.instance.AddPerlinShake(10f, 0.5f, 10f);
+                GUIManager.instance.ClimbJump();
+            }
+        }
+    }
 }
--- src/CanadianCuisine/controllers/CuisineAfflictionCharacter.cs
+++ src/CanadianCuisine/controllers/CuisineAfflictionCharacter.cs
@@ -1,6 +1,10 @@
 ﻿using CanadianCuisine.data;
 using Peak.Afflictions;
 using Photon.Pun;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CanadianCuisine.controllers;
 
@@ -9,6 +13,8 @@ public class CuisineAfflictionCharacter : MonoBehaviourPunCallbacks
     private Character character;
     private bool hasHighJump = false;
     private float _originalJumpImpulse;
+
+    public bool HasHighJump => hasHighJump;
 
     public float highJumpMultiplier = 2f;
     private CharacterMovement m_characterMovement;
@@ -24,7 +30,8 @@ public class CuisineAfflictionCharacter : MonoBehaviourPunCallbacks
     {
         hasHighJump = false;
 
-        if (character.refs.afflictions.HasAfflictionType(CuisineAfflictionManager.TypeByName(CuisineAfflictionValues.HIGH_JUMP_NAME), out var affliction) &&
+        if (character.refs.afflictions.HasAfflictionType(
+                CuisineAfflictionManager.TypeByName(CuisineAfflictionValues.HIGH_JUMP_NAME), out var affliction) &&
             affliction is AfflictionHighJump affliction_HighJump)
         {
             hasHighJump = true;
@@ -34,5 +41,64 @@ public class CuisineAfflictionCharacter : MonoBehaviourPunCallbacks
         m_characterMovement.jumpImpulse =
             hasHighJump ? _originalJumpImpulse * highJumpMultiplier : _originalJumpImpulse;
     }
-  
+
+    [PunRPC]
+    public void HighJumpRpc(bool isPalJump, float jumpImpulse)
+    {
+        float staminaCostMult = 1f;
+        float jumpMult = 1f;
+        Vector3 jumpDir = Vector3.up;
+        if (isPalJump)
+        {
+            staminaCostMult = 0f;
+            jumpMult = 2f;
+            character.data.sincePalJump = 0f;
+            jumpDir += character.data.lookDirection_Flat * 0.25f;
+            foreach (var sfxInstance in m_characterMovement.boostPlayer)
+            {
+                sfxInstance.Play(character.Center);
+            }
+        }
+
+        character.data.jumpsRemaining--;
+        character.data.isCrouching = false;
+        character.data.chargingJump = true;
+        character.OnStartJump();
+        StartCoroutine(IDoJump());
+
+        IEnumerator IDoJump()
+        {
+            _ = jumpImpulse;
+            if (!character.OutOfStamina())
+            {
+                character.refs.afflictions.SubtractStatus(CharacterAfflictions.STATUSTYPE.Web, 0.05f, fromRPC: true);
+            }
+
+            float c = 0f;
+            while (c < 0.1f)
+            {
+                character.data.sinceGrounded = 0f;
+                character.data.sinceJump = 0f;
+                c += Time.deltaTime;
+                yield return null;
+            }
+
+            character.OnJump();
+            character.data.chargingJump = false;
+            character.data.isJumping = true;
+            bool flag = character.GetTotalStamina() > m_characterMovement.jumpStaminaUsageSprinting &&
+                        character.input.sprintIsPressed;
+
+            character.data.sprintJump = flag;
+            character.UseStamina(
+                (flag ? m_characterMovement.jumpStaminaUsageSprinting : m_characterMovement.jumpStaminaUsage) *
+                staminaCostMult, flag);
+            foreach (var part in character.refs.ragdoll.partList)
+            {
+                // We override the jump impulse to use the boosting impulse. This way, boosting another player while in high jump gives an even bigger boost
+                part.AddForce(jumpDir * (jumpImpulse * jumpMult * m_characterMovement.balloonJumpMultiplier),
+                    ForceMode.Acceleration);
+            }
+        }
+    }
 }
