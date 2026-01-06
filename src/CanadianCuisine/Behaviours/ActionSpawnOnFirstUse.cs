ae34c3eaf62d9c7a14c9e6e647f24c7f148a151b
MiiMii1205 <boldje1.work@gmail.com>
2026-01-06 17:32:10 -0500

    Updated translations

---
+++ src/CanadianCuisine/Behaviours/ActionSpawnOnFirstUse.cs
@@ -0,0 +1,15 @@
+﻿namespace CanadianCuisine.Behaviours;
+
+public class ActionSpawnOnFirstUse : Action_Spawn
+{
+    public override void RunAction()
+    {
+        var uses = item.GetData<OptionableIntItemData>(DataEntryKey.ItemUses);
+        
+        if (!uses.HasData || uses.Value == item.totalUses)
+        {
+            base.RunAction();
+        }
+        
+    }
+}
--- src/CanadianCuisine/Behaviours/CookingBehaviours/CookingBehaviourChangeFeedbackSfx.cs
+++ src/CanadianCuisine/Behaviours/CookingBehaviours/CookingBehaviourChangeFeedbackSfx.cs
@@ -10,13 +10,14 @@ public class CookingBehaviourChangeFeedbackSfx : AdditionalCookingBehavior
         {
             Plugin.Log.LogWarning("Cooking Behaviour will try to change to an empty sound effect name.");
         }
+
     }
 
     public override void TriggerBehaviour(int cookedAmount)
     {
         var uses = itemCooking.item.gameObject.GetComponents<ItemUseFeedback>();
 
-        ItemUseFeedback? mainFeedback = itemCooking.item._useFeedback;
+        var mainFeedback = itemCooking.item._useFeedback;
         ItemUseFeedback? nextFeedback = null;
 
         foreach (var itemUseFeedback in uses)
